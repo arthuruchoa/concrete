a2dca7c9c1145aa68f58b1006e3dabc9a924b9a4
Arthur Uchoa <arthuruchoarj@gmail.com>
2016-04-07 19:52:17 -0300

    Enviando os primeiros arquivos

---
+++ Concrete/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Concrete.Startup))]
+namespace Concrete
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
